70f893a75aa6116e743620b2ba30937aeed7fae7
Manthan Kumbhar <kum.manthan@gmail.com>
2021-07-04 14:27:20 +0530

    initial commit + README

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Psyduck from "../src/psyduck.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <h2 class="heading-secondary">Sigh bot</h2>
+      <br />
+      <img src={Psyduck} alt="Psyduck" />
+      <br />
+      <a
+        href="https://github.com/manthankumbhar/sigh_bot_www/blob/master/README.md"
+        class="btn btn--white btn--animated"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Check docs
+      </a>
+      <br />
+      <a
+        href="https://discord.com/oauth2/authorize?client_id=848918677193228318&permissions=8&scope=bot"
+        class="btn btn--green btn--animated"
+        style={{ marginTop: "3rem" }}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Get our bot to your discord server!
+      </a>
+      <br />
+      <p className="p-tag">
+        Developed by{" "}
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://manthankumbhar.netlify.app/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
-      </header>
+          Manthan Kumbhar
+        </a>{" "}
+        &copy; {new Date().getFullYear()} (All Rights Reserved)
+      </p>
     </div>
   );
 }
